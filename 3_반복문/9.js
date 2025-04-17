84aa5e6cdac7b6d32387bc89497a94fcdcb04eb8
whitepanguin <dun04045@gmail.com>
2025-04-17 13:21:11 +0900

    undefined error

--- 3_반복문/9.js
+++ 3_반복문/9.js
@@ -1,5 +1,12 @@
 const fs = require("fs");
-// let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-const input = fs.readFileSync("./input.txt").toString().split(" ");
+// let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+const input = fs.readFileSync("3_반복문/input.txt").toString().split(" ");
 
 console.log(input);
+let box = [];
+for (let i = 0; i < input; i++) {
+  for (let j = 0; j < i + 1; j++) {
+    box[i] += "*";
+  }
+}
+console.log(box);
