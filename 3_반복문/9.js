b25e10d85dab258468d1ed80148f217532e7d000
whitepanguin <dun04045@gmail.com>
2025-04-17 09:07:17 +0900

    fixed

---
+++ 3_반복문/9.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+// let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+const input = fs.readFileSync("./input.txt").toString().split(" ");
+
+console.log(input);
