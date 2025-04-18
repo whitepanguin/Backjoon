10d0971870e08bb966141a38e62874c3aa500876
whitepanguin <dun04045@gmail.com>
2025-04-18 09:01:45 +0900

    3_q11

--- 3_반복문/11.js
+++ 3_반복문/11.js
@@ -1,16 +1,33 @@
 const fs = require("fs");
-// let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
-const input = fs.readFileSync("3_반복문/input.txt").toString().split(" ");
+let input = require("fs").readFileSync("/dev/stdin").toString().split(/\r\n/);
+// const input = fs.readFileSync("3_반복문/input.txt").toString().split(/\r\n/);
 
-let inputNum = Number(input);
-let box = [];
+// console.log(input);
 
-for (let i = 0; i < inputNum; i++) {
-  box[i] = "";
-  for (let j = inputNum - 1; j >= 0; j--) {
-    j > i ? (box[i] += " ") : (box[i] += "*");
+for (let i = 0; i < input.length; i++) {
+  let total = 0;
+  input[i] = input[i].split(" ");
+  total = Number(input[i][0]) + Number(input[i][1]);
+
+  if (total == 0) {
+    break;
+  } else {
+    console.log(total);
   }
 }
-for (let n = 0; n < inputNum; n++) {
-  console.log(box[n]);
+// ??? 제출이 틀렸다고 한다,,?
+var fs = require("fs");
+var input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+for (let i = 0; i < input.length; i++) {
+  let A = parseInt(input[i].split(" ")[0]);
+  let B = parseInt(input[i].split(" ")[1]);
+  let result = A + B;
+
+  if (result === 0) {
+    break;
+  } else {
+    console.log(result);
+  }
 }
+// 반대로 이건 맞는다고 한다.. 흠
