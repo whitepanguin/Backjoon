fb6f26d26fcf46571912f6e4a3a9f18917af42ce
whitepanguin <dun04045@gmail.com>
2025-04-21 09:02:10 +0900

    4_q3 found it

---
+++ 4_1차원 배열/3.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const file =
+  process.platform === "linux" ? "dev/stdin" : "4_1차원 배열/input.txt";
+const input = fs.readFileSync(file).toString().trim().split("\r\n");
+
+// console.log(input);
+const num = input[0];
+const arr = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+console.log(`${arr[0]} ${arr[num - 1]}`);
+// 요번에도 타입에러,, 하지만! 왜 이런 현상이 이러났는지 알아냈다! .split("\r\n") 에서 \r을 인식 못하는거 같다
