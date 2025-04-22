66b260283461f12bf0a9b516d4daf1c609e051e6
whitepanguin <dun04045@gmail.com>
2025-04-22 09:11:37 +0900

    4_q4 different

--- 4_1차원 배열/4.js
+++ 4_1차원 배열/4.js
@@ -16,3 +16,10 @@ for (let i = 1; i < arr.length; i++) {
 }
 console.log(hig);
 console.log(num);
+// 역시 다른 방법 있었다. 더 쉬운 방법
+// let input = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(x=> Number(x));
+
+let max = Math.max(...input);
+
+console.log(max);
+console.log(input.indexOf(max) + 1);
