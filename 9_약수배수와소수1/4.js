c153c18d512e244950c3d4b4d870a39fb6c3717c
whitepanguin <dun04045@gmail.com>
2025-06-27 08:23:32 +0900

    9_4q_2

--- 9_약수배수와소수1/4.js
+++ 9_약수배수와소수1/4.js
@@ -4,15 +4,23 @@ const file =
 
 const input = fs.readFileSync(file).toString().trim().split(/\r?\n/);
 
-console.log(input);
+// console.log(input);
 
 let leng = Number(input[0]);
 let arr = input[1].split(" ").map(Number);
 
+let count = 0;
 for (let i = 0; i < leng; i++) {
+  let od = 0;
   let num = arr[i];
-  for (let j = 0; j < num; j++) {
-    if (j % num == 0) {
+  for (let j = 1; j < num; j++) {
+    // console.log(num % j == 0);
+    if (num % j == 0) {
+      od++;
     }
   }
+  if (od == 1) {
+    count++;
+  }
 }
+console.log(count);
