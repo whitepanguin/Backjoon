485c1a125d9f2a0b752c1868d6d79104803e6d3c
whitepanguin <dun04045@gmail.com>
2025-07-25 08:41:54 +0900

    10_q8

---
+++ 10_기하1/8.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const file = process.platform === "linux" ? "dev/stdin" : "10_기하1/input.txt";
+
+const input = fs.readFileSync(file).toString().trim().split(/\r?\n/);
+
+// console.log(input);
+const [a, b, c] = input[0]
+  .split(" ")
+  .map(Number)
+  .sort((x, y) => x - y);
+
+if (a + b > c) {
+  console.log(a + b + c);
+} else {
+  console.log((a + b) * 2 - 1);
+}
